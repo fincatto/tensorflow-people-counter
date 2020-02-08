615d4c4ffc5c92a6a636283f26dc992e7dd51192
Diego Fincatto <diego@fincatto.com>
2020-02-08 09:30:06 -0300

    Adicionado captura de video

--- wc2.py
+++ wc2.py
@@ -1,17 +1,15 @@
 import cv2
 import sys
-import logging as log
+#import logging as log
 import datetime as dt
 from time import sleep
 
-def main():
-    cascPath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(cascPath)
-    log.basicConfig(filename='wc2.log',level=log.INFO)
+def start_detection():
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    #log.basicConfig(filename='wc2.log',level=log.INFO)
 
     video_capture = cv2.VideoCapture(0)
     anterior = 0
-
     while True:
         if not video_capture.isOpened():
             print('Unable to load camera.')
@@ -20,7 +18,6 @@ def main():
 
         # Capture frame-by-frame
         ret, frame = video_capture.read()
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = faceCascade.detectMultiScale(
@@ -36,22 +33,19 @@ def main():
 
         if anterior != len(faces):
             anterior = len(faces)
-            log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
-
+            #log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+            print("Faces: "+str(len(faces))+" em "+str(dt.datetime.now()))
 
         # Display the resulting frame
         cv2.imshow('Video', frame)
 
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) == 27: 
             break
 
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
-
     # When everything is done, release the capture
+    print("Releasing capture...")
     video_capture.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main()
+    start_detection()
--- webcam.py
+++ webcam.py
@@ -1,39 +1,39 @@
-import os
+#import os
 import cv2
-import tarfile
-import tensorflow as tf
-import six.moves.urllib as urllib
+#import tarfile
+#import tensorflow as tf
+#import six.moves.urllib as urllib
 
 # Models can bee found here: https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md
-MODEL_FILE = 'ssd_inception_v2_coco_2017_11_17.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-DOWNLOAD_DIR = 'downloads/'
-DOWNLOAD_FILE = DOWNLOAD_DIR + MODEL_FILE
+#MODEL_FILE = 'ssd_inception_v2_coco_2017_11_17.tar.gz'
+#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+#DOWNLOAD_DIR = 'downloads/'
+#DOWNLOAD_FILE = DOWNLOAD_DIR + MODEL_FILE
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = DOWNLOAD_DIR + 'frozen_inference_graph.pb'
+#PATH_TO_CKPT = DOWNLOAD_DIR + 'frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+#PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 # Number of classes to detect
-NUM_CLASSES = 90
+#NUM_CLASSES = 90
 
 # Download Model
-if not os.path.exists(PATH_TO_CKPT):
-    print ('Criando diretorio do model:', DOWNLOAD_DIR)
-    if not os.path.exists(DOWNLOAD_DIR):
-        os.makedirs(DOWNLOAD_DIR)
-
-    print ('Iniciando download do model:',  DOWNLOAD_FILE)
-    opener = urllib.request.URLopener()
-    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, DOWNLOAD_FILE)
-    tar_file = tarfile.open(DOWNLOAD_FILE)
-    for file in tar_file.getmembers():
-        file_name = os.path.basename(file.name)
-        if 'frozen_inference_graph.pb' in file_name:
-            print ('Estraindo arquivo em', PATH_TO_CKPT)
-            tar_file.extract(file, PATH_TO_CKPT)
+#if not os.path.exists(PATH_TO_CKPT):
+#    print ('Criando diretorio do model:', DOWNLOAD_DIR)
+#    if not os.path.exists(DOWNLOAD_DIR):
+#        os.makedirs(DOWNLOAD_DIR)
+#
+#    print ('Iniciando download do model:',  DOWNLOAD_FILE)
+#    opener = urllib.request.URLopener()
+#    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, DOWNLOAD_FILE)
+#    tar_file = tarfile.open(DOWNLOAD_FILE)
+#    for file in tar_file.getmembers():
+#        file_name = os.path.basename(file.name)
+#        if 'frozen_inference_graph.pb' in file_name:
+#            print ('Estraindo arquivo em', PATH_TO_CKPT)
+#            tar_file.extract(file, PATH_TO_CKPT)
 
 # Define the video stream
 #print ('Iniciando captura de video...')
@@ -41,24 +41,27 @@ if not os.path.exists(PATH_TO_CKPT):
 #print(cap)
 
 
-def show_webcam(mirror=False):
+def show_webcam():
     print ('Iniciando captura de video...')
     cam = cv2.VideoCapture(0)
     while True:
-        ret_val, img = cam.read()
+        ret, img = cam.read() #captura a imagem da webcam
+        img = cv2.flip(img, 1) #gira imagem para parecer um espelho
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #transforma a imagem em escala de cinza
         
-        if mirror: 
-            img = cv2.flip(img, 1)
+        #para cada foto capturada da webcam, exibo em uma tela para analise
+        cv2.imshow('CV2 Video Capture', img)
         
-        cv2.imshow('my webcam', img)
-        
-        # esc to quit
+        # esc para sair
         if cv2.waitKey(1) == 27: 
-            break  
+            print ('Gravando ultima imagem capturada em /tmp/img.png...')
+            cv2.imwrite('/tmp/img.png',img) #grava a imagem capturada
+            break #sai do loop  
     
+    # libera os recursos
+    cam.release()
     cv2.destroyAllWindows()
 
 
-
 if __name__ == '__main__':
-    show_webcam(mirror=True)
+    show_webcam()
