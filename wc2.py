911af3e458ddaae788bc14e482cf8f74c6103f33
Diego Fincatto <diego@fincatto.com>
2020-02-15 12:44:24 -0300

    Adicionado sistema de logging

--- wc2.py
+++ wc2.py
@@ -1,51 +1,61 @@
 import cv2
 import sys
-#import logging as log
+import logging as log
 import datetime as dt
 from time import sleep
 
+def config_logging():
+    formatter = log.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] - %(message)s")
+    rootLogger = log.getLogger()
+
+    fileHandler = log.FileHandler("/tmp/wc2.log")
+    fileHandler.setFormatter(formatter)
+    rootLogger.addHandler(fileHandler)
+
+    consoleHandler = log.StreamHandler()
+    consoleHandler.setFormatter(formatter)
+    rootLogger.addHandler(consoleHandler)
+
 def start_detection():
-    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    #log.basicConfig(filename='wc2.log',level=log.INFO)
+    log.debug("Iniciando sistema de identificacao de faces...")
+    try:
+        cap = cv2.VideoCapture(0)
+        anterior = 0
+        while True:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    video_capture = cv2.VideoCapture(0)
-    anterior = 0
-    while True:
-        if not video_capture.isOpened():
-            print('Unable to load camera.')
-            sleep(5)
-            pass
+            faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
 
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30)
-        )
+            if anterior != len(faces):
+                anterior = len(faces)
+                #log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+                print("Faces: "+str(len(faces))+" em "+str(dt.datetime.now()))
 
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
 
-        if anterior != len(faces):
-            anterior = len(faces)
-            #log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
-            print("Faces: "+str(len(faces))+" em "+str(dt.datetime.now()))
-
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
-
-        if cv2.waitKey(1) == 27: 
-            break
-
-    # When everything is done, release the capture
-    print("Releasing capture...")
-    video_capture.release()
-    cv2.destroyAllWindows()
+            #sai da tela com esc
+            if cv2.waitKey(1) == 27: 
+                break
+    except Exception as e:
+        log.error('Erro ao rodar sistema: '+ str(e))
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+    config_logging()
     start_detection()
+    
